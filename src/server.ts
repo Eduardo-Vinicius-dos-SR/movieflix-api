3dcce22465c7596cd1184327e610fccb76b46733
Eduardo-Vinicius-dos-SR <eduardovdsr@gmail.com>
2025-11-05 18:40:12 -0300

    adicionada correção de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -23,11 +23,19 @@ app.get("/movies", async (_, res) => {
 });
 
 app.post("/movies", async (req, res) => {
-	// console.log(`Conteúdo do body enviado na requisição: ${req.body.title}`);
-
 	const { title, genre_id, language_id, oscar_count, release_date } = req.body;
 
 	try {
+		const movieWithSameTitle = await prisma.movie.findFirst({
+			where: {
+				title: { equals: title, mode: "insensitive" },
+			},
+		});
+
+		if (movieWithSameTitle) {
+			return res.status(409).send({ message: "Já existe um filme cadastrado com esse título" });
+		}
+
 		await prisma.movie.create({
 			data: {
 				title,
