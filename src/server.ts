8199f122fc3a478de73174ceae8d7d0f971b00d7
Eduardo-Vinicius-dos-SR <eduardovdsr@gmail.com>
2025-11-06 17:53:04 -0300

    Adicionado o filtro de filmes por gênero

--- src/server.ts
+++ src/server.ts
@@ -104,6 +104,29 @@ app.delete("/movies/:id", async (req, res) => {
 	}
 });
 
+app.get("/movies/:genreName", async (req, res) => {
+	try {
+		const moviesFilteredByGenreName = await prisma.movie.findMany({
+			include: {
+				genres: true,
+				languages: true,
+			},
+			where: {
+				genres: {
+					name: {
+						equals: req.params.genreName,
+						mode: "insensitive",
+					},
+				},
+			},
+		});
+
+		res.status(200).send(moviesFilteredByGenreName);
+	} catch (er) {
+		return res.status(500).send({ message: "Falha ao filtrar filmes por gênero" });
+	}
+});
+
 app.listen(port, () => {
 	console.log(`Servidor em execução na porta ${port}`);
 });
